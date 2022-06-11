4d18921ade10480b18193126ae228bc6ebca0dee
anirudh <anirudh07.anand@gmail.com>
2022-06-11 06:10:52 +0530

    restructure directory

--- Code/OCR/Handwritten OCR/Third Party/ez.py
+++ Code/OCR/Handwritten OCR/Third Party/ez.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 import easyocr
 
-reader = easyocr.Reader(['en'],gpu = False) # load once only in memory.
+reader = easyocr.Reader(['en'],gpu = True) # load once only in memory.
 
-image_file_name='ocr_test_img_2.jpg' 
+image_file_name='../ocr_test_img.jpg' 
 image = cv2.imread(image_file_name)
 
 # sharp the edges or image.
@@ -13,6 +13,4 @@ sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
 sharpen = cv2.filter2D(gray, -1, sharpen_kernel)
 thresh = cv2.threshold(sharpen, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 r_easy_ocr=reader.readtext(thresh,detail=0)
-
-cv2.imshow(image)
-cv2.waitKey()
+print(r_easy_ocr)
